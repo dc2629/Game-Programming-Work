3f787fe3e23153d3f54ff134a2dd36908ccbf91b
Daniel Chang <danielchang@live.com>
2014-09-20 18:19:26 -0400

    not workinh

--- GPAssignment1/Assignment1/Source.cpp
+++ GPAssignment1/Assignment1/Source.cpp
@@ -1,5 +1,3 @@
-//#pragma once //For headers usually.
-
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <SDL_image.h>
@@ -46,8 +44,7 @@ void DrawSprite(GLint texture, float x, float y, float rotation) {
 	glDisable(GL_TEXTURE_2D);//Disable the texture since OpenGl won't use the same texture when redrawing other quads.
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
 	//Main Setup
 	SDL_Init(SDL_INIT_VIDEO);//Initializes SDL
 	displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);//Creates the window with OpenGL and the dimensions of the window.
@@ -81,7 +78,8 @@ int main(int argc, char *argv[])
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {//If the Window is closed or the user quits the program, end the loop.
 				done = true;
-			} else if(event.type = SDL_MOUSEMOTION){
+			}
+			else if (event.type = SDL_MOUSEMOTION){
 				float unitX = (((float)event.motion.x / 800.0f)*2.66f) - 1.33f;
 				float unitY = (((600.0f - (float)event.motion.y) / 600.0f)*2.0f) - 1.0f;
 				posX = unitX;
@@ -90,7 +88,7 @@ int main(int argc, char *argv[])
 		}
 		glClearColor(55.0f / 255.0f, 84.0f / 255.0f, 229.0f / 255.0f, 1.0f);//Determines default coloring
 		glClear(GL_COLOR_BUFFER_BIT);//Makes background default color
-		
+
 		DrawSprite(bMeteor, 0, .5, 0);
 		float ticks = (float)SDL_GetTicks() / 1000.0f;
 		float elapsed = ticks - lastFrameTicks;
@@ -106,16 +104,17 @@ int main(int argc, char *argv[])
 		glDisableClientState(GL_COLOR_ARRAY);
 
 		const Uint8 *keys = SDL_GetKeyboardState(NULL);
-			if (keys[SDL_SCANCODE_LEFT]) {
-			
-			}
-			else if (keys[SDL_SCANCODE_RIGHT]) {
-				// go right! 
+		if (keys[SDL_SCANCODE_LEFT]) {
 
-		SDL_GL_SwapWindow(displayWindow);//Something about there being two windows, swap the one that is visible and the one that is being programmed.
+		}
+		else if (keys[SDL_SCANCODE_RIGHT]) {
+			// go right! 
+
+			SDL_GL_SwapWindow(displayWindow);//Something about there being two windows, swap the one that is visible and the one that is being programmed.
+		}
+
+		SDL_Quit();
+
+		return 0;
 	}
-
-	SDL_Quit();
-
-	return 0;
 }
